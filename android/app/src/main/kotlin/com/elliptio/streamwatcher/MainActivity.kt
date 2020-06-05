fa6582b9f32bf4b6b9083e64f810f5642c4fb8bf
Philip Townsend <phil@mobxdev.com>
2020-06-05 17:12:13 -0400

    initial commit - list view showing states

---
+++ android/app/src/main/kotlin/com/elliptio/streamwatcher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.elliptio.streamwatcher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/state_picker.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:streamwatcher/constants.dart';
+import 'dart:core';
+
+class StatePicker extends StatefulWidget {
+  final String title;
+  StatePicker({this.title});
+  _StatePicker createState() => _StatePicker();
+}
+
+class _StatePicker extends State<StatePicker> {
+
+  final entries = ['Aimee', 'Kelly', 'Elizabeth'];
+  final states = kAllStates;
+
+  ListView showStates() {
+
+    var stateKeys = states.keys;
+
+    ListView lv = ListView.separated (
+      padding: EdgeInsets.all(10.0),
+      itemCount: stateKeys.length,
+      itemBuilder: (BuildContext context, int index) {
+        String key = states.keys.elementAt(index);
+        return new Column(
+          children: <Widget>[
+            new ListTile(
+              title: new Text("${states[key]}"),
+            )
+          ],
+        );
+      }, separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
+
+    return lv;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title)
+      ),
+      body: showStates().build(context),
+    );
+  }
+}
