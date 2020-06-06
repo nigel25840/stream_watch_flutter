3fef46ec1c7e69ad575aa4ce9c6416faa3553879
Philip Townsend <phil@mobxdev.com>
2020-06-05 21:10:10 -0400

    Fixed duplicate gauges for gauge picker view

--- lib/data_service.dart
+++ lib/data_service.dart
@@ -1,20 +1,35 @@
+import 'dart:convert';
 import 'dart:core';
+import 'package:flutter/rendering.dart';
 import 'package:http/http.dart';
 
 import 'package:streamwatcher/constants.dart';
+import 'package:streamwatcher/gauge_model.dart';
 
 // let url = URL(string: "\(Constants.BASE_URL)&stateCd=\(state)&parameterCd=00060,00065&siteType=ST&siteStatus=all")!
 
+// GAUGE NAME:          value.timeSeries[0].sourceInfo.siteName
+// GAUGE  IDENTIFIER:   value.timeSeries[0].sourceInfo.siteCode[0].value
+
 class DataService {
-
-  Future<Map<String, dynamic>> stateGauges(String stateAbbr) async {
-
-    String url = '$kBaseUrl&stateCd=$stateAbbr&parameterCd=00060,00065&siteType=ST&siteStatus=all';
+  Future<List<GaugeModel>> stateGauges(String stateAbbr) async {
+    String url =
+        '$kBaseUrl&stateCd=$stateAbbr&parameterCd=00060,00065&siteType=ST&siteStatus=all';
     Response res = await get(url);
+    var json = jsonDecode(res.body);
+    var gaugeList = List<GaugeModel>();
+    var idSet = Set<String>();
+    int count = json['value']['timeSeries'].length;
 
-    print(res.body);
-
-    return null;
+    for (int index = 0; index < count; index++) {
+      var item = json['value']['timeSeries'][index]['sourceInfo'];
+      var gID = item['siteCode'][0]['value'];
+      var name = item['siteName'];
+      if (!idSet.contains(gID)) {
+        gaugeList.add(GaugeModel(gaugeName: name, gaugeState: stateAbbr, gaugeId: gID));
+      }
+      idSet.add(gID);
+    }
+    return gaugeList;
   }
-
 }
