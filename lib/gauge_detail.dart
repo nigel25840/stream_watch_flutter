771cd87b23ffb5f06b8b184baaa05f0f5bee39d6
Philip Townsend <phil@mobxdev.com>
2020-06-23 16:31:54 -0400

    Set five ticks for measure axis

--- lib/gauge_detail.dart
+++ lib/gauge_detail.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:streamwatcher/constants.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'dart:core';
@@ -23,9 +24,21 @@ class _GaugeDetail extends State<GaugeDetail> {
   String getGauge() {
     return widget.gaugeId;
   }
-  List<charts.Series<GaugeFlowReading, double>> _seriesLineData;
+  List<charts.Series<GaugeFlowReading, double>> _seriesFlowData;
+  List<charts.Series<GaugeStageReading, double>> _seriesStageData;
   List<GaugeFlowReading> gaugeFlowReadings = [];
   List<GaugeStageReading> gaugeStageReadings  = [];
+  List<int>tickValues = [];
+  var isCfs = true;
+
+  int currentCfs = 0;
+  int lowCfs = 0;
+  int highCfs = 0;
+  double currentStage = 0.0;
+  double lowStage = 0.0;
+  double highStage = 0.0;
+
+  List<int> cfsTickVals = [];
 
   _getGaugeData() async {
     // 03185400
@@ -42,25 +55,60 @@ class _GaugeDetail extends State<GaugeDetail> {
         _getStageReadings(timeseries[index]['values'][0]['value']);
       }
     }
+    _generateChartFlowData();
   }
 
-  List<GaugeFlowReading> _getFlowReadings(json) {
+  _generateChartFlowData() {
+    _seriesFlowData = List<charts.Series<GaugeFlowReading, double>>();
+    _seriesFlowData.add(
+      charts.Series(
+        colorFn: (__, _) => charts.ColorUtil.fromDartColor(Color(0xff0000ff)),
+        areaColorFn: (__, _) => charts.ColorUtil.fromDartColor(Color(0xff45b6fe)),
+        id:'Redings',
+        data: gaugeFlowReadings,
+        domainFn: (GaugeFlowReading reading, _) => reading.timestamp,
+        measureFn: (GaugeFlowReading reading, _) => reading.flow
+      )
+    );
+  }
+
+  _getFlowReadings(json) {
+    tickValues = [];
     for (int i = 0; i < json.length; i++) {
       var dict = json[i];
       var value = int.parse(dict['value']);
       var timestamp = DateTime.parse(dict['dateTime']);
       gaugeFlowReadings.add(GaugeFlowReading(value, i.toDouble()));
-      print("OBJECT: ${value} - ${timestamp}");
+      tickValues.add(value);
     }
+    currentCfs = tickValues.last;
+    tickValues.sort();
+    lowCfs = tickValues.first;
+    highCfs = tickValues.last;
+
+    tickValues = _setTickValues(highCfs, lowCfs, 5);
   }
 
-  List<GaugeStageReading> _getStageReadings(json){
+  List<int> _setTickValues(int high, int low, int numberOfTicks) {
+    double increase = 1.1;
+    int _low = low;
+    int _high = (high * 1.1).toInt();
+    int total = _high - _low;
+    int delta = (total / numberOfTicks).toInt();
+    List<int> retval = [_low];
+    for (int i = 1; i < numberOfTicks; i++) {
+      _low += delta;
+      retval.add(_low);
+    }
+    return retval;
+  }
+
+  _getStageReadings(json){
     for (int i = 0; i < json.length; i++) {
       var dict = json[i];
       var value = double.parse(dict['value']);
       var timestamp = DateTime.parse(dict['dateTime']);
       gaugeStageReadings.add(GaugeStageReading(value, timestamp));
-      print("OBJECT: ${value} - ${timestamp}");
     }
   }
 
@@ -73,6 +121,7 @@ class _GaugeDetail extends State<GaugeDetail> {
 
   @override
   Widget build(BuildContext context) {
+    _generateChartFlowData();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.gaugeName),
@@ -83,14 +132,28 @@ class _GaugeDetail extends State<GaugeDetail> {
               Container (
                 height: MediaQuery.of(context).size.height * .6,
                 alignment: Alignment(0.0, 0.0),
-                color: Colors.orange,
-                child: Text(widget.gaugeId),
+                color: Colors.lightBlueAccent,
+                child: charts.LineChart(
+                  _seriesFlowData,
+                  primaryMeasureAxis: charts.NumericAxisSpec(
+                    tickProviderSpec: charts.StaticNumericTickProviderSpec(
+                      <charts.TickSpec<num>>[
+                        charts.TickSpec<num>(tickValues[0]),
+                        charts.TickSpec<num>(tickValues[1]),
+                        charts.TickSpec<num>(tickValues[2]),
+                        charts.TickSpec<num>(tickValues[3]),
+                        charts.TickSpec<num>(tickValues[4]),
+                      ]
+                    )
+                  ),
+                ),
               ),
               Container (
                 width: double.infinity,
                 color: Colors.white,
-                alignment: Alignment(0.0, -1.0),
+                alignment: Alignment(-1.0, 0.0),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Text("CONSTRAINED BOX"),
                       ConstrainedBox(
