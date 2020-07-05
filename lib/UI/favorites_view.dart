3ae84733fafa1ef9af4f571b1f5d27058941e78a
Philip Townsend <phil@mobxdev.com>
2020-07-05 17:04:24 -0400

    Added and stubbed favorites view class

---
+++ lib/UI/favorites_view.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/cupertino.dart';
+
+class FavoritesView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FavoritesView();
+}
+
+class _FavoritesView extends State<FavoritesView> {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
