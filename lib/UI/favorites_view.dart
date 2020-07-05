624975be6e7b22a0a22ccc7b412a845950244863
Philip Townsend <phil@mobxdev.com>
2020-07-05 17:19:09 -0400

    Added FavoritesView to drawer

--- lib/UI/favorites_view.dart
+++ lib/UI/favorites_view.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:streamwatcher/UI/drawer.dart';
 
 class FavoritesView extends StatefulWidget {
   @override
@@ -9,6 +10,12 @@ class FavoritesView extends StatefulWidget {
 class _FavoritesView extends State<FavoritesView> {
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorites'),
+      ),
+      body: Center(child: Text('Favorites')),
+      endDrawer: RFDrawer(),
+    );
   }
 }
