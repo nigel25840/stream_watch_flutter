e0519943cfae9f0204ef5d506a2ba0c71cd1021e
Philip Townsend <phil@mobxdev.com>
2020-07-15 17:54:06 -0400

    favorites list view integrated

--- lib/UI/favorites_view.dart
+++ lib/UI/favorites_view.dart
@@ -31,8 +31,6 @@ class _FavoritesView extends State<FavoritesView> {
     return list;
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,19 +60,61 @@ class FavoriteCell extends StatefulWidget {
 
 class _FavoriteCell extends State<FavoriteCell> {
   var _cellData;
+  TextStyle style = TextStyle(fontWeight: FontWeight.bold, fontSize: 15);
 
   Future<GaugeModel> _getFavorite() async {
     _cellData = await DataProvider().gaugeJson(widget.favoriteGaugeId, 1);
     var timeSeries = _cellData['value']['timeSeries'];
-    //value.timeSeries[0].sourceInfo.siteName
-    print('SITE NAME');
-    print(timeSeries[0]['sourceInfo']['siteName']);
-    return await GaugeModel(gaugeName: timeSeries[0]['sourceInfo']['siteName'], gaugeState: 'DC', gaugeId: '1234');
+    double lastFlowReading;
+    double lastStageReading;
+
+    // TODO: add exception handling here
+//    for (int index = 0; index < timeSeries.length; index++) {
+//      String varName = timeSeries[index]['variable']['variableName'];
+//      if (varName.toLowerCase().contains('streamflow')) {
+//        try{
+//          var flowVal = timeSeries[index]['values'][0]['value'][0]['value'];
+//          lastFlowReading = double.parse(flowVal);
+//          print('*********************\n LAST FLOW: $flowVal');
+//        } catch (e) {
+//          print('EXCEPTION: ${e.toString()}');
+//        }
+//      }
+//    }
+
+
+    for (int index = 0; index < timeSeries.length; index++) {
+      String varName = timeSeries[index]['variable']['variableName'];
+      try {
+        if (varName.toLowerCase().contains('streamflow')) {
+          var flowVal = timeSeries[index]['values'][0]['value'][0]['value'];
+          lastFlowReading = double.parse(flowVal);
+        } else if (varName.toLowerCase().contains('gage')) {
+          var stageVal = timeSeries[index]['values'][0]['value'][0]['value'];
+          lastStageReading = double.parse(stageVal);
+        }
+      } catch (e) {
+
+      }
+    }
+
+    GaugeModel model = await GaugeModel(
+        gaugeName: timeSeries[0]['sourceInfo']['siteName'],
+        gaugeState: 'DC',
+        gaugeId: '1234');
+    model.lastFlowReading = lastFlowReading;
+    model.lastStageReading = lastStageReading;
+
+    return model;
   }
 
   Card _faveCardView(AsyncSnapshot snapshot, BuildContext context) {
     GaugeModel model = snapshot.data;
     var card = Card(
+      elevation: 2,
+      color: Colors.tealAccent,
+      shadowColor: Colors.black,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -85,10 +125,24 @@ class _FavoriteCell extends State<FavoriteCell> {
               children: [
                 Text(model.gaugeName),
                 Text(model.gaugeState),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('${model.lastFlowReading != null ? model.lastFlowReading.round().toString() + 'cfs' : 'N/A'}', style: style),
+                      Text('${model.lastStageReading != null ? model.lastStageReading.toString() + 'ft' : 'N/A'}', style: style),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
-          Icon(Icons.arrow_forward)
+          Row(
+            children: [
+              Icon(Icons.arrow_forward),
+            ],
+          ),
         ],
       ),
     );
@@ -101,13 +155,15 @@ class _FavoriteCell extends State<FavoriteCell> {
       future: _getFavorite(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         //GaugeModel fave = snapshot.data;
-        if(snapshot.connectionState == ConnectionState.done) {
+        if (snapshot.connectionState == ConnectionState.done) {
           return _faveCardView(snapshot, context); //Text(fave.gaugeName);
         } else {
-          return Align(child: CircularProgressIndicator(), widthFactor: 2,);
+          return Align(
+            child: CircularProgressIndicator(),
+            widthFactor: 2,
+          );
         }
       },
     );
   }
-
 }
