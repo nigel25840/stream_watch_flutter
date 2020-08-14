e9ba1303500a36b677490d3a37356980fad125a0
Philip Townsend <phil@mobxdev.com>
2020-08-14 10:47:50 -0400

    Added favorite model

---
+++ lib/model/favorite_model.dart
@@ -0,0 +1,15 @@
+
+class FavoriteModel {
+  final String favoriteId;
+  final String favoriteName;
+
+  double currentFlow;
+  double currentStage;
+  bool increasing;
+  DateTime lastUpdated;
+
+  Map<String, double> prefStage = {'high': null, 'low': null};
+  Map<String, double> prefFlow = {'high': null, 'low': null};
+
+  FavoriteModel(this.favoriteId, this.favoriteName);
+}
