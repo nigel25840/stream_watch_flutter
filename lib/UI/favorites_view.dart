8895cb7d6f95d64df3296b415addbc8da9ff7eaf
Philip Townsend <phil@mobxdev.com>
2020-09-02 14:41:53 -0400

    Added progress indicator for reloading favorites

--- lib/UI/favorites_view.dart
+++ lib/UI/favorites_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:progress_dialog/progress_dialog.dart';
 import 'package:provider/provider.dart';
 import 'package:streamwatcher/UI/drawer.dart';
 import 'package:streamwatcher/UI/favorite_listview_cell.dart';
@@ -24,11 +25,14 @@ class _FavoritesView extends State<FavoritesView> {
     super.initState();
   }
 
-  void _refreshButtonTapped() async {
+  Future<void>  _refreshButtonTapped() async {
+    final ProgressDialog prog = ProgressDialog(context);
+    prog.style(
+      message: "Refreshing from USGS..."
+    );
+    prog.show();
     refreshAll = true;
-    setState(() {
-      viewModel.refreshAllFavorites();
-    });
+    await viewModel.refreshAllFavorites().then((value) => { prog.hide() });
   }
 
   Future<void> _loadData() async {
@@ -62,11 +66,13 @@ class _FavoritesView extends State<FavoritesView> {
               children: [
                 FlatButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
                         return HelpView();
                       }));
                     },
-                    padding: EdgeInsets.only(left: 20, right: 20, top:10, bottom: 10),
+                    padding: EdgeInsets.only(
+                        left: 20, right: 20, top: 10, bottom: 10),
                     color: Colors.lightBlueAccent,
                     textColor: Colors.white,
                     shape: RoundedRectangleBorder(
@@ -80,9 +86,17 @@ class _FavoritesView extends State<FavoritesView> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Icon(Icons.help, size: 28,),
-                          SizedBox(width: 8,),
-                          Text('Open Help...', style: style,)
+                          Icon(
+                            Icons.help,
+                            size: 28,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Open Help...',
+                            style: style,
+                          )
                         ],
                       ),
                     ))
@@ -98,6 +112,7 @@ class _FavoritesView extends State<FavoritesView> {
   Widget build(BuildContext context) {
     bool reload = refreshAll;
     refreshAll = false;
+
 
     if (viewModel.favoriteModels.length < 1) {
       return Consumer<FavoritesViewModel>(
@@ -123,7 +138,11 @@ class _FavoritesView extends State<FavoritesView> {
                       String gaugeId = model.favorites[index];
                       Key key = Key(model.favorites[index]);
                       //return FavoriteCard(gaugeId, key, reload, true);
-                      return FavoriteCell(gaugeId: gaugeId, key: UniqueKey(), isDismissable: true, reload: reload);
+                      return FavoriteCell(
+                          gaugeId: gaugeId,
+                          key: UniqueKey(),
+                          isDismissable: true,
+                          reload: reload);
                     },
                   ),
                   onRefresh: _loadData,
@@ -135,13 +154,11 @@ class _FavoritesView extends State<FavoritesView> {
         child: Icon(Icons.refresh),
         onPressed: () {
           setState(() {
-            refreshAll = true;
             _refreshButtonTapped();
           });
         },
       ),
     );
-
   }
 
 //  void _onReorder(int oldIndex, int newIndex) {
