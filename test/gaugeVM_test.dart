21ca8a9534bc3192ccdc71d8ef514f56e9396ded
Philip Townsend <phil@mobxdev.com>
2020-10-27 13:04:00 -0400

    added a unit test file

--- test/gaugeVM_test.dart
+++ test/gaugeVM_test.dart
@@ -1,9 +1,13 @@
-
-import 'package:streamwatcher/main.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:streamwatcher/dataServices/data_provider.dart';
 
-void main() {
-  test('View model can produce ultimate values for high, low, cfs, and stage', () {
-
-  });
-}
+// void main() {
+//  group('Given data provider returns json', (){
+//    test('View model can produce ultimate values for high, low, cfs, and stage for New River at Thurmond', () {
+//       DataProvider().fetchGaugeDetail('03185400').then((value) {
+//         print(value);
+//       });
+//    });
+//  });
+// }
