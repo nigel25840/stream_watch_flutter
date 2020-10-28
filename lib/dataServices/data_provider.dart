fdbea1f07e4ae095d88d338a35a2c371cb80e9dc
Philip Townsend <phil@mobxdev.com>
2020-10-28 12:45:14 -0400

    gauge selector vm updates

--- lib/dataServices/data_provider.dart
+++ lib/dataServices/data_provider.dart
@@ -1,19 +1,8 @@
 import 'dart:convert';
-import 'dart:core';
 import 'package:http/http.dart';
-
-import 'package:streamwatcher/Util/constants.dart';
-import 'package:streamwatcher/model/gauge_detail_model.dart';
-import 'package:streamwatcher/model/gauge_model.dart';
 import 'package:codable/codable.dart';
 
-// let url = URL(string: "\(Constants.BASE_URL)&stateCd=\(state)&parameterCd=00060,00065&siteType=ST&siteStatus=all")!
-
-// GAUGE NAME:          value.timeSeries[0].sourceInfo.siteName
-// GAUGE  IDENTIFIER:   value.timeSeries[0].sourceInfo.siteCode[0].value
-
 class DataProvider {
-
   Future<T> fetchFromUrl<T extends Coding>(String url, T model) async {
     Response res = await get(url);
     final readingJson = json.decode(res.body);
--- lib/viewModel/gauge_selector_viewmodel.dart
+++ lib/viewModel/gauge_selector_viewmodel.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
 import 'package:streamwatcher/Util/constants.dart';
 import 'package:streamwatcher/dataServices/data_provider.dart';
 import 'package:streamwatcher/model/gauge_model.dart';
 
 class GaugeSelectorViewModel extends ChangeNotifier {
   List<GaugeReferenceModel> stateList;
-  String stateAbbreviation;
+  List<GaugeReferenceModel> gaugeModels = [];
 
-  GaugeSelectorViewModel(this.stateAbbreviation);
+  GaugeSelectorViewModel();
 
-  Future<GaugeRefModel> poplulate() async {
-    String url = '$kBaseUrl&stateCd=$stateAbbreviation&parameterCd=00060,00065&siteType=ST&siteStatus=all';
-    return await DataProvider().fetchFromUrl<GaugeRefModel>(url, GaugeRefModel());
+  Future<void> poplulateVM({String state}) async {
+    gaugeModels.clear();
+    String url = '$kBaseUrl&stateCd=$state&parameterCd=00060,00065&siteType=ST&siteStatus=all';
+    await DataProvider().fetchFromUrl<GaugeRefModel>(url, GaugeRefModel()).then((model) {
+      model.value.timeSeries.forEach((ts) { 
+        GaugeReference ref = ts.sourceInfo;
+        gaugeModels.add(GaugeReferenceModel(gaugeName: ref.siteName, gaugeId: ref.gaugeId));
+      });
+
+      final ids = gaugeModels.map((e) => e.gaugeId).toSet();
+      gaugeModels.retainWhere((element) => ids.remove(element.gaugeId));
+      Comparator<GaugeReferenceModel> sortByName = (a, b) => a.gaugeName.compareTo(b.gaugeName);
+      gaugeModels.sort(sortByName);
+      notifyListeners();
+    });
   }
 }
