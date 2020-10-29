1c00cbfac7c49a7d403306eb6c316e47056976cf
Philip Townsend <phil@mobxdev.com>
2020-10-29 13:51:42 -0400

    added comments to unit tests

--- test/gaugeVM_test.dart
+++ test/gaugeVM_test.dart
@@ -11,6 +11,8 @@ void main() {
   group('DataProvider returns correct model objects', () {
 
     test('View model can produce a valid GaugeRefModel for a state (WV)', () async {
+      // mimic the following url
+      // https://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=WV&parameterCd=00060,00065&siteType=ST&siteStatus=all
       String url = 'http://localhost:3001/getWVGauges'; // Mockoon API
       GaugeRefModel m = await DataProvider().fetchFromUrl(url, GaugeRefModel());
       expect((m == null), false);
@@ -18,6 +20,9 @@ void main() {
     });
 
     test('View model produces a valid GaugeReadingModel for a particular gauge ID', () async {
+
+      // mimic the following url
+      // https://waterservices.usgs.gov/nwis/iv/?format=json&site=03185400&period=PT72H
       String url = 'http://localhost:3001/getRiver'; // Mockoon API
       GaugeReadingModel rm = await DataProvider().fetchFromUrl(url, GaugeReadingModel());
       expect((rm == null), false);
@@ -30,7 +35,6 @@ void main() {
       expect((info == null), false);
       expect((info.siteName == null), false);
       expect((info.siteCode == null), false);
-
     });
 
   });
